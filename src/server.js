dbc25e7061e33ed5646917003980f364688caee9
Raushan <raushankumar606@gmail.com>
2020-08-11 21:52:42 +0530

    lession 1

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
+import cors from 'cors'
+
+export const app = express()
+
+app.disable('x-powered-by')
+
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+app.get('/', (req, res, next) => {
+  res.send({ message: 'HELLO' })
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'Okay' })
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is on 3000')
+  })
+}
