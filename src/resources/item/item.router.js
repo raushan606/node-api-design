a1629ade038424198937eb3ea8026a11ad28f6b9
Raushan <raushankumar606@gmail.com>
2020-08-12 17:18:56 +0530

    controller item

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,19 +1,19 @@
 import { Router } from 'express'
-const controller = (req, res) => {
-  res.send({ message: 'hello' })
-}
+import controllers from './item.controllers'
 
 const router = Router()
 
+// /api/item
 router
   .route('/')
-  .get(controller)
-  .post(controller)
+  .get(controllers.getMany)
+  .post(controllers.createOne)
 
+// /api/item/:id
 router
   .route('/:id')
-  .get(controller)
-  .put(controller)
-  .delete(controller)
+  .get(controllers.getOne)
+  .put(controllers.updateOne)
+  .delete(controllers.removeOne)
 
 export default router
