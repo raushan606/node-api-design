b8a79e824e439a670a783bb02d033440b114e79f
Raushan <raushankumar606@gmail.com>
2020-08-12 23:55:21 +0530

    signin

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -23,14 +23,38 @@ export const signup = async (req, res) => {
   try {
     const user = await User.create(req.body)
     const token = newToken(user)
-    res.send({ token })
+    return res.status(201).send({ token })
   } catch (e) {
     console.error(e)
     return res.status(400).end()
   }
 }
 
-export const signin = async (req, res) => {}
+export const signin = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send({
+      message: 'Email and password required'
+    })
+  }
+
+  const user = await User.findOne({ email: req.body.email }).exec()
+
+  if (!user) {
+    return res.status(401).send({ message: 'Not auth' })
+  }
+
+  try {
+    const match = await user.checkPassword(req.body.password)
+    if (!match) {
+      return res.status(401).send({ message: 'Not auth' })
+    }
+    const token = newToken(user)
+    return res.status(201).send({ token })
+  } catch (e) {
+    console.error(e)
+    return res.status(401).send({ message: 'Not auth' })
+  }
+}
 
 export const protect = async (req, res, next) => {
   next()
