53967a54c30d06e07244bc5049d87e6dbf3bd828
Raushan <raushankumar606@gmail.com>
2020-08-13 00:06:22 +0530

    auth protect middleware

--- src/server.js
+++ src/server.js
@@ -4,8 +4,10 @@ import morgan from 'morgan'
 import cors from 'cors'
 import config from './config'
 import { connect } from 'mongoose'
-
+import userRouter from './resources/user/user.router'
+import listRouter from './resources/list/list.router'
 import itemRouter from './resources/item/item.router'
+import { signup, signin, protect } from './utils/auth'
 
 export const app = express()
 
@@ -15,8 +17,13 @@ app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
+app.post('/signup', signup)
+app.post('/sigin', signin)
 
+app.use('/api', protect)
+app.use('/api/user', userRouter)
 app.use('/api/item', itemRouter)
+app.use('/api/list', listRouter)
 
 export const start = async () => {
   try {
